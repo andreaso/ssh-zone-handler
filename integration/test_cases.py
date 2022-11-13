e66f19ea2352e01e55c51bc4e38480a63fe59744
Andreas Olsson <andreas@arrakis.se>
2022-11-13 20:52:32 +0100

    WIP: generalize a bit more

---
+++ integration/test_cases.py
@@ -0,0 +1,10 @@
+cases = [
+    {
+        "name": "help output",
+        "command": "help",
+        "zones": [],
+        "rc": 0,
+        "stdout": "usage: command [ZONE]\n\nhelp\t\t\tDisplay this help message\nlist\t\t\tList available zones\ndump ZONE\t\tOutput full content of ZONE\nlogs [ZONE1 ZONE2]\tOutput the last five days' log entries for ZONE(s)\nretransfer ZONE\t\tTrigger a full (AXFR) retransfer of ZONE\nstatus ZONE\t\tShow ZONE status\n",
+        "stderr": "",
+    }
+]

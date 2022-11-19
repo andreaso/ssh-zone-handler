d511daa87541c0a1dd41c4552543fd8cd4e7c297
Andreas Olsson <andreas@arrakis.se>
2022-11-19 11:43:33 +0100

    WIP: some more testing

--- integration/test_cases.py
+++ integration/test_cases.py
@@ -1,10 +1,41 @@
-cases = [
-    {
-        "name": "help output",
-        "command": "help",
-        "zones": [],
-        "rc": 0,
-        "stdout": "usage: command [ZONE]\n\nhelp\t\t\tDisplay this help message\nlist\t\t\tList available zones\ndump ZONE\t\tOutput full content of ZONE\nlogs [ZONE1 ZONE2]\tOutput the last five days' log entries for ZONE(s)\nretransfer ZONE\t\tTrigger a full (AXFR) retransfer of ZONE\nstatus ZONE\t\tShow ZONE status\n",
-        "stderr": "",
-    }
+from dataclasses import dataclass
+
+
+@dataclass
+class TestCase:
+    name: str
+    command: str
+    zones: list[str]
+    stdout: str
+    stderr: str = ""
+    rc: int = 0
+
+
+cases: list[TestCase] = [
+    TestCase(
+        name="help output",
+        command="help",
+        zones=[],
+        stdout="usage: command [ZONE]\n\nhelp\t\t\tDisplay this help message\nlist\t\t\tList available zones\ndump ZONE\t\tOutput full content of ZONE\nlogs [ZONE1 ZONE2]\tOutput the last five days' log entries for ZONE(s)\nretransfer ZONE\t\tTrigger a full (AXFR) retransfer of ZONE\nstatus ZONE\t\tShow ZONE status\n",
+    ),
+    TestCase(
+        name="listing zones",
+        command="list",
+        zones=[],
+        stdout="example.com\nexample.net\n",
+    ),
+    TestCase(
+        name="dummy command",
+        command="bazinga",
+        zones=["example.com"],
+        stdout="",
+        stderr='Invalid command, try "help"\n',
+        rc=1,
+    ),
+    TestCase(
+        name="triggering retransfer",
+        command="retransfer",
+        zones=["example.com"],
+        stdout='Triggering retransfer of zone "example.com"\n',
+    ),
 ]
